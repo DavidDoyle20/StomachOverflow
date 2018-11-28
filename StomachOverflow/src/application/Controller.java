5133c0158703516b4d72b1a7c42475441c459a88
Quafleteezy <45133446+Quafleteezy@users.noreply.github.com>
2018-11-27 20:02:09 -0600

    Add files via upload

--- StomachOverflow/src/application/Controller.java
+++ StomachOverflow/src/application/Controller.java
@@ -1,10 +1,10 @@
 package application;
 
+import java.awt.TextField;
 import java.io.IOException;
 import application.ScenceStack;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
@@ -16,9 +16,11 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import java.util.HashMap;
 
 public class Controller implements Initializable {
-
+	
+	private HashMap<String, Account> accounts = new HashMap<String, Account>();
 	private Button back;
 	private Button faq;
 	private Button help;
@@ -27,11 +29,17 @@ public class Controller implements Initializable {
 	private Button reviews;
 	private Button logInHome;
 	private Button homeLogIn;
+	private TextField userNameLogIn;
+	private TextField passwordLogIn;
+	private TextField userNameSignUp;
+	private TextField passwordSignUp;
+	private TextField passwordSignUpConfirm;  
 	
 	protected DSLinkedStack<Scene> sceneStack;
 
 	public void initialize(URL arg0V, ResourceBundle arg1) {
 		sceneStack = new DSLinkedStack<Scene>();
+	
 	}
 
 	public void logInPage(ActionEvent e) {
@@ -63,7 +71,7 @@ public class Controller implements Initializable {
 	public void search(ActionEvent e) {
 		Parent root;
 		try {
-			root = FXMLLoader.load(getClass().getResource("RestList.fxml"));
+			root = FXMLLoader.load(getClass().getResource("Rest.fxml"));
 			ScenceStack.push(((Node) e.getSource()).getScene());
 			Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
 			stage.setScene(new Scene(root, 1280, 720));
@@ -143,5 +151,9 @@ public class Controller implements Initializable {
 		stage.setScene(ScenceStack.top());
 		ScenceStack.pop();
 		
+	}
+	
+	public void logIn() {
+		System.out.println(userNameSignUp.getText());
 	}
 }
